581a474d3a05123d4bfd4c510f884306590a1086
Giovanni Lituma <giovanninet@icloud.com>
2022-08-26 15:36:37 -0400

    Add some dummy data

---
+++ seedData.js
@@ -0,0 +1,45 @@
+const users = [
+  {
+    "name": "charmander",
+    "email": "charmander@multiverse.com"
+  },
+  {
+    "name": "squirtle",
+    "email": "squirtle@multiverse.io",
+  },
+]
+
+const boards = [
+  {
+    "name": "French",
+    "description": "French cheese",
+    "rating": 5,
+  },
+  {
+    "name": "Italian",
+    "description": "Italian cheese board",
+    "rating": 5,
+  },
+  {
+    "name": "Swiss",
+    "description": "Swiss cheese board",
+    "rating": 5,
+  },
+]
+
+const cheeses = [
+  {
+    name: 'provolone',
+    description: 'provolone description'
+  },
+  {
+    name: 'swiss',
+    description: 'swiss cheese'
+  }
+]
+
+module.exports = {
+  users,
+  boards,
+  cheeses,
+};
