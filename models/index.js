701632789d33ff23b43f95aa5e54ccf68d7de8fd
Giovanni Lituma <giovanninet@icloud.com>
2022-08-30 15:54:48 -0400

    Board - User relationship is tested

--- models/index.js
+++ models/index.js
@@ -2,4 +2,13 @@ const { User } = require('./User')
 const { Board } = require('./Board')
 const { Cheese } = require('./Cheese')
 
+// Multiple Boards can be added to a User.
+User.hasMany(Board, { 
+  foreignKey: 'userId',
+})
+Board.belongsTo(User)
+
+Board.belongsToMany(Cheese, { through: 'BoardCheese', foreignKey: 'boardId' })
+Cheese.belongsToMany(Board, { through: 'BoardCheese', foreignKey: 'cheeseId' })
+
 module.exports = { User, Board, Cheese }
